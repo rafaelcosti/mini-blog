e9dd30b662648e7c6c6665057690aa8b4e2bb352
Rafael Costi <rafaelcosti@outlook.com>
2023-02-09 23:41:49 -0300

    Add route 404

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,6 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { useAuthentication } from '../../hook/authentication/useAuthentication';
 import ILogin from './ILogin';
 import styles from './Login.module.scss';
 
@@ -8,9 +10,88 @@ import styles from './Login.module.scss';
  * @returns 
  */
 const Login = (prop: ILogin) : ReactElement => {
+  /**
+   */
+  const [eMail, setEMail] = useState("");
+  
+  /**
+   * 
+   */
+  const [password, setPassword] = useState("");
+  
+  /**
+   * 
+   */
+  const [errorMessage, setErrorMessage] = useState("");
+
+  /**
+   * Hook auth
+   */    
+  const { error: authError, loading } = useAuthentication();
+
+  /**
+   * 
+   */
+  useEffect(() => {
+    if (authError) {
+      setErrorMessage(authError);
+    }
+  }, [authError]);
+  
+  /**
+   * 
+   * @param e 
+   */
+  const handleLoginSubmit = async (e: React.FormEvent) => {
+
+  }
+
   return (
     <div className="sbx-container">
-      <h1>Login</h1>
+      <Row className='sbx-row'>
+        <Col>
+          <h1>Login</h1>
+        </Col>
+      </Row>
+      <Row className='sbx-row'>
+        <Col>
+          <form onSubmit={handleLoginSubmit}>
+            <div className="form-control">
+              <label htmlFor="name">
+                E-mail:
+              </label>
+              <input type="text" required={true} name="email" placeholder="Informe seu e-mail"
+                     value={eMail}
+                     onChange={(e: React.ChangeEvent<HTMLInputElement> ) => setEMail(e.target.value)} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="password">
+                Senha:
+              </label>
+              <input type="password" required={true} name="password" placeholder="Informe sua senha"
+                     value={password} id="password"
+                     onChange={(e: React.ChangeEvent<HTMLInputElement> ) => setPassword(e.target.value)} />
+            </div>
+            {
+              errorMessage ?
+                <div className="form-control">
+                  <div className="error">
+                    { errorMessage }
+                  </div>
+                </div>
+              :
+               <></>
+            }
+            <div className="form-control">
+              { loading ? 
+                  <button className='button' disabled>Aguarde...</button>
+                : 
+                  <button className='button'>Entrar</button>
+              }
+            </div>
+          </form>
+        </Col>
+      </Row>
     </div>
   );
 }
